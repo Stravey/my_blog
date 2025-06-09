aa46f19608a151c9d954b152f463ef8750c9b037
Stravey <2205487298@qq.com>
2025-06-09 22:09:12 +0800

    update dao 数据库访问

--- src/main/java/com/liu/blog/dao/UserRepository.java
+++ src/main/java/com/liu/blog/dao/UserRepository.java
@@ -1,5 +1,11 @@
 package com.liu.blog.dao;
 
 
-public interface UserRepository {
+import com.liu.blog.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsernameAndPassword(String username,String password);
+
 }
