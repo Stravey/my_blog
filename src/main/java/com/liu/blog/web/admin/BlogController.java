8f06210c3d6c038ea7323cbe9e9ed5ff18e3f8db
Stravey <2205487298@qq.com>
2025-06-09 22:10:56 +0800

    update web

--- src/main/java/com/liu/blog/web/admin/BlogController.java
+++ src/main/java/com/liu/blog/web/admin/BlogController.java
@@ -1,14 +1,16 @@
-package com.liu.blog.web;
+package com.liu.blog.web.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class BlogController {
 
-    @GetMapping("/blog")
-    public String blog() {
-        return "blog";
+    @GetMapping("/blogs")
+    public String list() {
+        return "admin/blogs";
     }
 
 }
--- src/main/java/com/liu/blog/web/admin/LoginController.java
+++ src/main/java/com/liu/blog/web/admin/LoginController.java
@@ -1,5 +1,50 @@
 package com.liu.blog.web.admin;
 
 
+import com.liu.blog.pojo.User;
+import com.liu.blog.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/admin")
 public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String loginPage(){
+        return "admin/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        HttpSession session,
+                        RedirectAttributes attributes) {
+        User user = userService.checkUser(username,password);
+        if(user != null){
+            user.setPassword(null);
+            session.setAttribute("user",user);
+            return "admin/index";
+        } else {
+            attributes.addFlashAttribute("message","用户名和密码错误");
+            // 重定向登陆失败再次返回登录页面
+            return "redirect:admin/login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/admin";
+    }
+
 }
