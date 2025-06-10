6eaa536e84fb7453651a933d118f0a98dcb1ede3
Stravey <2205487298@qq.com>
2025-06-10 20:57:25 +0800

    update service

--- src/main/java/com/liu/blog/service/impl/TypeServiceImpl.java
+++ src/main/java/com/liu/blog/service/impl/TypeServiceImpl.java
@@ -1,5 +1,57 @@
 package com.liu.blog.service.impl;
 
 
-public class TypeServiceImpl {
+import com.liu.blog.dao.TypeRepository;
+import com.liu.blog.exception.NotFoundException;
+import com.liu.blog.pojo.Type;
+import com.liu.blog.service.TypeService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TypeServiceImpl implements TypeService {
+
+    @Autowired
+    private TypeRepository typeRepository;
+
+    @Transactional
+    @Override
+    public Type saveType(Type type) {
+        return typeRepository.save(type);
+    }
+
+    @Transactional
+    @Override
+    public Type getType(Long id) {
+        return typeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("不存在该类型"));
+    }
+
+    @Transactional
+    @Override
+    public Page<Type> listType(Pageable pageable) {
+        return typeRepository.findAll(pageable);
+    }
+
+    @Transactional
+    @Override
+    public Type updateType(Long id,Type type) {
+        Type t = typeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("不存在Id"));
+        BeanUtils.copyProperties(type, t);
+        return typeRepository.save(t);
+    }
+
+    @Transactional
+    @Override
+    public void deleteType(Long id) {
+        typeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("不存在Id"));
+    }
+
 }
--- src/main/java/com/liu/blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/liu/blog/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ package com.liu.blog.service.impl;
 import com.liu.blog.dao.UserRepository;
 import com.liu.blog.pojo.User;
 import com.liu.blog.service.UserService;
-import com.liu.blog.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +15,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username,String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
-        return user;
+        return userRepository.findByUsernameAndPassword(username, password);
     }
 
 }
