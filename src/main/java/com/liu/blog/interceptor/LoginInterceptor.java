cca46914a1a49d0e7163b2baa495279a74cb3d6f
Stravey <2205487298@qq.com>
2025-06-10 19:46:31 +0800

    update interceptor

--- src/main/java/com/liu/blog/interceptor/LoginInterceptor.java
+++ src/main/java/com/liu/blog/interceptor/LoginInterceptor.java
@@ -1,5 +1,28 @@
 package com.liu.blog.interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        if(request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/admin");
+            return false;
+        }
+        return true;
+    }
+
 }
