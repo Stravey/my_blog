d99ac8a5ca095c775b82fc90af6e1e0ee5e43827
Stravey <2205487298@qq.com>
2025-06-10 20:58:22 +0800

    update repository

--- src/main/java/com/liu/blog/dao/TypeRepository.java
+++ src/main/java/com/liu/blog/dao/TypeRepository.java
@@ -1,5 +1,11 @@
 package com.liu.blog.dao;
 
 
-public interface TypeRepository {
+import com.liu.blog.pojo.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type, Long> {
+
+
+
 }
