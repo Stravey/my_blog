95151e8bed912c9ad15c251fa2aa71d1ccaa0f48
Stravey <2205487298@qq.com>
2025-06-10 20:56:40 +0800

    update service

--- src/main/java/com/liu/blog/service/TypeService.java
+++ src/main/java/com/liu/blog/service/TypeService.java
@@ -1,5 +1,20 @@
 package com.liu.blog.service;
 
 
+import com.liu.blog.pojo.Type;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
 public interface TypeService {
+
+    Type saveType(Type type);
+
+    Type getType(Long id);
+
+    Page<Type> listType(Pageable pageable);
+
+    Type updateType(Long id,Type type);
+
+    void deleteType(Long id);
 }
