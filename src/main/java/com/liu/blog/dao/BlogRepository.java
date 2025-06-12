3be776d92232bb613cc840ffc788973fe273d77a
Stravey <2205487298@qq.com>
2025-06-12 21:01:42 +0800

    update dao

--- src/main/java/com/liu/blog/dao/BlogRepository.java
+++ src/main/java/com/liu/blog/dao/BlogRepository.java
@@ -1,5 +1,9 @@
 package com.liu.blog.dao;
 
 
-public interface BlogRepository {
+import com.liu.blog.pojo.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface BlogRepository extends JpaRepository<Blog, Long> , JpaSpecificationExecutor<Blog> {
 }
