a7661328aad076511bcf6924507b3538dc6ca0c1
Stravey <2205487298@qq.com>
2025-06-14 22:25:50 +0800

    update post method

--- src/main/java/com/liu/blog/web/admin/BlogController.java
+++ src/main/java/com/liu/blog/web/admin/BlogController.java
@@ -1,10 +1,12 @@
 package com.liu.blog.web.admin;
 
 import com.liu.blog.pojo.Blog;
+import com.liu.blog.pojo.User;
 import com.liu.blog.service.BlogService;
 import com.liu.blog.service.TagService;
 import com.liu.blog.service.TypeService;
 import com.liu.blog.vo.BlogQuery;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -33,7 +36,7 @@ public class BlogController {
     private TagService tagService;
 
     @GetMapping("/blogs")
-    public String blogs(@PageableDefault(size = 2,sort = {"updateTime"},direction = Sort.Direction.DESC) Pageable pageable,
+    public String blogs(@PageableDefault(size = 2,sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
                         BlogQuery blog, Model model) {
         model.addAttribute("type", typeService.listType());
         model.addAttribute("page",blogService.listBlog(pageable,blog));
@@ -58,5 +61,20 @@ public class BlogController {
         model.addAttribute("tag",tagService.listTag());
         model.addAttribute("blog",new Blog());
         return INPUT;
+    }
+
+    @PostMapping("/blogs")
+    public String post(Blog blog, RedirectAttributes attributes, HttpSession session) {
+        blog.setUser((User)session.getAttribute("user"));
+        blog.setType(typeService.getType(blog.getType().getId()));
+        blog.setTags(tagService.listTag(blog.getTagIds()));
+
+        Blog b = blogService.saveBlog(blog);
+        if(b == null) {
+            attributes.addFlashAttribute("message","新增失败!");
+        } else {
+            attributes.addFlashAttribute("message","新增成功!");
+        }
+        return REDIRECT_LIST;
     }
 }
