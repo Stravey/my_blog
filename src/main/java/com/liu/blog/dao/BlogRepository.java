af0ff22e5df0d9545e3c89334118dad48919ce84
Stravey <2205487298@qq.com>
2025-06-22 20:36:47 +0800

    modify before create

--- src/main/java/com/liu/blog/dao/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.liu.blog.dao;
-
-
-import com.liu.blog.pojo.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> , JpaSpecificationExecutor<Blog> {
-}
---
+++ src/main/java/com/liu/blog/entity/User.java
@@ -0,0 +1,5 @@
+package com.liu.blog.entity;
+
+
+public class User {
+}
