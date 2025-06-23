bda9b68ecf161784e2df719b908297f80908c67c
Stravey <2205487298@qq.com>
2025-06-23 21:34:20 +0800

    update aspect

--- src/main/java/com/liu/blog/aspect/LogAspect.java
+++ src/main/java/com/liu/blog/aspect/LogAspect.java
@@ -1,5 +1,79 @@
 package com.liu.blog.aspect;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+
+/**
+ * 日志切面
+ *
+ * @author Strive
+ * @version 1.0
+ * @since 2025-06-23
+ */
+@Aspect
+@Component
 public class LogAspect {
+
+    /**
+     * 请求信息内部类
+     */
+    private  class RequestLog {
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+
+        public RequestLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "RequestLog{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("execution(* com.liu.blog.controller.*.*(..))")
+    private void log() {}
+
+    @Around("log()")
+    public Object aroundPrintLog(ProceedingJoinPoint point) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURI();
+        String ip = request.getRemoteAddr();
+        String classMethod = point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName();
+        Object[] args = point.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+
+        logger.info("Request : {}",requestLog);
+
+        Object result = point.proceed(args);
+
+        logger.info("Result : {}",result);
+
+        return result;
+    }
 }
