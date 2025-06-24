3884e86be408f946c2ac48545f62c9082c10e88e
Stravey <2205487298@qq.com>
2025-06-24 21:03:15 +0800

    update service

--- src/main/java/com/liu/blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/liu/blog/service/impl/UserServiceImpl.java
@@ -1,5 +1,32 @@
 package com.liu.blog.service.impl;
 
 
-public class UserServiceImpl {
+import com.liu.blog.dao.UserDao;
+import com.liu.blog.entity.User;
+import com.liu.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User服务层
+ *
+ * @author Strive
+ * @version 1.0
+ * @since 2025-06-24
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User checkUser(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return null;
+    }
 }
