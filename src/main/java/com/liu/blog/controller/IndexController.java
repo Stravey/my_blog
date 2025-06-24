a0ce4ca638450b468a1873a0b51ed2c5cba6714b
Stravey <2205487298@qq.com>
2025-06-24 21:02:47 +0800

    update controller

--- src/main/java/com/liu/blog/controller/IndexController.java
+++ src/main/java/com/liu/blog/controller/IndexController.java
@@ -1,5 +1,24 @@
 package com.liu.blog.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * index控制
+ *
+ * @author Strive
+ * @version 1.0
+ * @since 2025-06-24
+ */
+@Controller
 public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+
+
 }
